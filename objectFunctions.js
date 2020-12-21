f3a9bb48431455581f754c77946cae0247edfe45
Benjamin Lewis <bslewis2@ncsu.edu>
2020-12-21 17:10:53 -0500

    made object functions it has size and getKeyByValue

---
+++ objectFunctions.js
@@ -0,0 +1,12 @@
+//object function
+function size(obj) {
+	var size = 0, key;
+	for (key in obj) {
+		if (obj.hasOwnProperty(key)) size++;
+	}
+	return size;
+};
+
+function getKeyByValue(object, value) {
+  return Object.keys(object).find(key => object[key] === value);
+}
--- sheet.js
+++ sheet.js
@@ -32,15 +32,6 @@ sheet.key = {
 				"lo" : 19,
 				"st" : 20
 			}
-			
-//key stuff
-sheet.key.size = function() {
-	var size = 0, key;
-	for (key in this) {
-		if (this.hasOwnProperty(key)) size++;
-	}
-	return size;
-};
 	
 //thinking stuff	
 sheet.think = function() {
@@ -57,7 +48,7 @@ sheet.playerAllKnown = function() {
 }
 
 sheet.cardKnown = function() {
-	for
+	
 }
 
 sheet.countCardFor = function(card,mark) {
